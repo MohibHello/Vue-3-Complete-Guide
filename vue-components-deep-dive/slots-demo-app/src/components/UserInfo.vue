7df0688002ea9a12159c376910721246f0ffed0f
Mohib <www.muhibkhan@gmail.com>
2022-02-16 19:44:11 +0530

    added slot impl

---
+++ vue-components-deep-dive/slots-demo-app/src/components/UserInfo.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+      <base-card>
+      <template #header>
+      <h3>{{ fullName }}</h3>
+      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+  <template>
+      <p>{{ infoText }}</p>
+  </template>
+      </base-card>
+  </section>
+</template>
+
+<script>
+import BaseBadge from "./BaseBadge.vue";
+import BaseCard from "./BaseCard.vue";
+
+export default {
+  components: {
+    BaseBadge,
+    BaseCard
+  },
+  props: ['fullName', 'infoText', 'role'],
+};
+</script>
