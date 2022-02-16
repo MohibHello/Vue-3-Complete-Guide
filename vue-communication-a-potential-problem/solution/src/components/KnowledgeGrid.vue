96171e6e43decb98ff441a37ed13667709982cea
Mohib <www.muhibkhan@gmail.com>
2022-02-16 17:36:04 +0530

    added solution for vue communication problem

---
+++ vue-communication-a-potential-problem/solution/src/components/KnowledgeGrid.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <knowledge-element
+      v-for="topic in topics"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
+    ></knowledge-element>
+  </ul>
+</template>
+
+<script>
+export default {
+  //if proivde is given in top level then inject works
+  //inject works like props to receive prop from top/higer level component
+  inject:['topics']
+};
+</script>
