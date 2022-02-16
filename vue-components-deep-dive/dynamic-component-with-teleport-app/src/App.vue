4b60d81bcb2266563966029da0bed2d8793f46ee
Mohib <www.muhibkhan@gmail.com>
2022-02-16 21:16:06 +0530

    Component,teleport, keep-alive added

---
+++ vue-components-deep-dive/dynamic-component-with-teleport-app/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <button @click="setSelectedComponent('active-goals')">Click</button>
+    <button @click="setSelectedComponent('managed-goals')">Click</button>
+    <!-- <active-goals v-if="selectedComponent==='active-goals'"></active-goals>
+    <managed-goals v-if="selectedComponent==='managed-goals'"></managed-goals> -->
+  <keep-alive>
+    <component :is="selectedComponent"></component>
+  </keep-alive>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import ManagedGoals from "./components/ManagedGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+
+export default {
+  components:{
+    TheHeader,
+    ManagedGoals,
+    ActiveGoals
+  },
+  data() {
+    return {
+      selectedComponent:'active-goals',
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+  methods:{
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    }
+  }
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ vue-components-deep-dive/dynamic-component-with-teleport-app/src/components/ManagedGoals.vue
@@ -0,0 +1,38 @@
+<template>
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal" />
+  <button @click="setGoal">Set Goals</button>
+  <teleport to="">
+    <error-alert v-if="inputIsValid">
+      <h1>Input is invalid</h1>
+      <p>please provide few characters</p>
+      <button @click="confirmError">Okay</button>
+    </error-alert>
+  </teleport>
+</template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsValid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === '') {
+        this.inputIsValid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsValid = false;
+    },
+  },
+};
+</script>
